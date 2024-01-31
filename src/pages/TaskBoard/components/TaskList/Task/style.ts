8f0edf95f09756fdb0271e7644f9f277ea203799
Alejandro Lucas <alejandrolucas161@gmail.com>
2024-01-30 22:16:25 -0300

    Fixing color

--- src/pages/TaskBoard/components/TaskList/Task/style.ts
+++ src/pages/TaskBoard/components/TaskList/Task/style.ts
@@ -12,12 +12,12 @@ export const TaskTextContaner = styled("div")`
 export const TaskText = styled("span")`
   min-height: 24px;
 
-  color: b5b5b5;
+  color: #b5b5b5;
   font-size: 14px;
 `;
 
 export const TaskTextarea = styled("textarea")`
-  color: b5b5b5;
+  color: #b5b5b5;
   font-size: 14px;
 
   background-color: rgba(119, 119, 119, 0.4);
@@ -28,7 +28,7 @@ export const TaskTextarea = styled("textarea")`
   outline: none;
 
   &::placeholder {
-    color: b5b5b5;
+    color: #b5b5b5;
   }
 
   &:active {
